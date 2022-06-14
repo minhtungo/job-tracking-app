6ae7bc8712a40f8d22bfaeadd1a50ad238ff4f9f
minhtungo <loducfc@gmail.com>
2022-06-13 22:02:09 -0400

    styled sign up

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -1,6 +1,56 @@
+import { useState, useEffect } from 'react';
+import FormRow from '../../components/form-row/FormRow';
+import Logo from '../../components/Logo/Logo';
+
+import { Wrapper } from './Register.styles';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <div>Register</div>
-  )
-}
-export default Register
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+};
+export default Register;
