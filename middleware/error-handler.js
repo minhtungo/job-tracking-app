48ea3b79f9a4c96567bba499cbb5dec2e5c5428c
minhtungo <loducfc@gmail.com>
2022-06-14 19:51:46 -0400

    implement hash password

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,23 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
   const defaultError = {
-    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    mgs: 'Something went wrong, try again later',
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong, try again later',
   };
-  err.status(defaultError.statusCode).json({ msg: err });
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',');
+  }
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `This email address already existed`;
+  }
+
+  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMiddleware;
