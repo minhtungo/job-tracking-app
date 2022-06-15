2f72c989a55eb431e3f22829e0969b1a52a6009f
minhtungo <loducfc@gmail.com>
2022-06-15 19:39:29 -0400

    setup sidebar

--- client/src/components/BigSideBar/BigSideBar.js
+++ client/src/components/BigSideBar/BigSideBar.js
@@ -1,5 +1,24 @@
 import { Wrapper } from './BigSideBar.styles';
+import { useAppContext } from './../../context/appContext';
+import Logo from '../Logo/Logo';
+import NavLinks from '../nav-links/NavLinks';
 
 export const BigSideBar = () => {
-  return <Wrapper>BigSideBar</Wrapper>;
+  const { showSidebar } = useAppContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
--- client/src/components/SmallSideBar/SmallSideBar.js
+++ client/src/components/SmallSideBar/SmallSideBar.js
@@ -1,6 +1,33 @@
+import { FaTimes } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+
+import { useAppContext } from '../../context/appContext';
+import Logo from '../Logo/Logo';
+
 import { Wrapper } from './SmallSideBar.styles';
+import links from './../../utils/links';
+import NavLinks from '../nav-links/NavLinks';
 
 export const SmallSideBar = () => {
-  return <Wrapper>SmallSideBar</Wrapper>;
-};
+  const { showSidebar, toggleSidebar } = useAppContext();
 
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
